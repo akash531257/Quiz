cccadb09b4daa0301ca72410d61f51158c42f9db
akash <akash531257@gmail.com>
2018-01-08 23:52:18 -0500

    first commit

---
+++ js/Quiz-Java-Fundamentals.js
@@ -0,0 +1,26 @@
+var init = { 
+     'questions': [ 
+      {
+           'question': 'What does SQL stand for?',
+           'answers': ['Strong Question Language', 'Structured Question Language','Structured Query Language'],
+       	   'correctAnswer': 3
+       },
+       {
+           'question': 'Which SQL statement is used to extract data from a database?',
+           'answers': ['SELECT', 'EXTRACT','GET'],
+       	   'correctAnswer': 1
+       }
+     ],
+     
+	  'resultComments' :  
+	  {
+		   perfect: 'You\'re awesome enough to just walk into Mordor, aren&#39;t you?',
+			 excellent: 'My PRECIOUS!! You were outstanding!',
+			 good: 'Good enough, but beware the power of the ring.',
+			 average: 'An orc would have performed just as good as you!',
+			 bad: 'Is your mind poisoned by the ring?',
+			 poor: 'Is your mind poisoned by the ring?',
+			 worst: 'What is this new trolling?!'
+	  }
+
+ };
